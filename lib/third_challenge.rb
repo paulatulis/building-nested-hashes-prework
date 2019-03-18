a5cbb993684194d1c17bcdf93b94a56ce51cf09d
Paula Tulis <tulis.paula@gmail.com>
2019-03-18 12:10:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,34 +2,34 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name = "Lord Montague"
+        :name = "Lord Montague",
         :age = "53"
         },
       :matriarch => {
-        :name = "Lady Montague"
+        :name = "Lady Montague",
         :age = "54"
         #your key/value pairs here
         },
       :hero => {
-       :name = "Romeo"
-        :age = "15"
+       :name = "Romeo",
+        :age = "15",
         :status = "alive"#your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name = "Lord Capulet"
+        :name = "Lord Capulet",
         :age = "50"
         #your key/value pairs here
         },
       :matriarch => {
-        :name = "Lady Capulet"
+        :name = "Lady Capulet",
         :age = "51"#your key/value pairs here
         },
       :heroine => {
-        :name = "Juliet"
-        :age = "15"
+        :name = "Juliet",
+        :age = "15",
         :status = "alive"#your key/value pairs here
         },
       :heroine_friends => []
